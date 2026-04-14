fa86bdc2516c32ed8a00b1b689f0d6d7750fb308
KumarSurajwebdeveloper <kr.surajkumar09@gmail.com>
2026-04-14 23:39:27 +0530

    git commit -m "first commit"

---
+++ src/Arrays/String/ReverseString.java
@@ -0,0 +1,42 @@
+package Arrays.String;
+
+public class ReverseString {
+
+    public static void main (String[] args){
+
+        String input = "ABCDEFGH";
+        char[] charArray = input.toCharArray();
+
+        int leftIndex = 0;
+        int rightIndex = input.length() -1;
+
+        while(leftIndex < rightIndex){
+            char temp = charArray[rightIndex];
+            charArray[rightIndex] = charArray[leftIndex];
+            charArray[leftIndex] = temp;
+
+            leftIndex++;
+            rightIndex--;
+        }
+        System.out.println("Reversed string is " + input);
+//      System.out.println("Reversed string is " + reverseString(input) );
+
+    }
+
+    public static String reverseString(String input){
+
+        char[] charArray = input.toCharArray();
+
+        int leftIndex =0;
+        int rightIndex = charArray.length -1;
+
+        while(leftIndex < rightIndex) {
+            char temp = charArray[leftIndex];
+            charArray[leftIndex] = charArray[rightIndex];
+            charArray[rightIndex] = temp;
+            leftIndex++;
+            rightIndex--;
+        }
+        return new String (charArray);
+    }
+}
