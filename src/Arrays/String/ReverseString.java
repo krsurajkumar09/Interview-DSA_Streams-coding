713b7955028a83e67303c62c14bbbf25fc4662b9
KumarSurajwebdeveloper <kr.surajkumar09@gmail.com>
2026-04-15 09:03:25 +0530

    Code Refactored in Reversed String

--- src/Arrays/String/ReverseString.java
+++ src/Arrays/String/ReverseString.java
@@ -18,8 +18,9 @@ public class ReverseString {
             leftIndex++;
             rightIndex--;
         }
-        System.out.println("Reversed string is " + input);
-//      System.out.println("Reversed string is " + reverseString(input) );
+        System.out.println("Original string is " + input);
+//        System.out.println("Reversed string is " + new String(charArray));
+      System.out.println("Reversed string is " + reverseString(input) );
 
     }
 
