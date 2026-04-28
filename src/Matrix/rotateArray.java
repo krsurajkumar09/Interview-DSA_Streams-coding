0efc8d456f935293adcbc3dd7ab10a4654bcc47a
KumarSurajwebdeveloper <kr.surajkumar09@gmail.com>
2026-04-28 23:11:49 +0530

    rotateArray.java added

---
+++ src/Matrix/rotateArray.java
@@ -0,0 +1,41 @@
+package Matrix;
+import java.util.Arrays;
+
+public class rotateArray {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        rotate(matrix);
+
+        // Print rotated matrix
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Step 1: Reverse rows
+        for (int i = 0; i < n / 2; i++) {
+            int[] temp = matrix[i];
+            matrix[i] = matrix[n - 1 - i];
+            matrix[n - 1 - i] = temp;
+        }
+
+        // Step 2: Transpose matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
